66bfba5a962eb5786efc6cec80c079e4bbb2a8e4
Hedi <sisxme@foxmail.com>
2020-01-02 23:36:48 +0800

    Initial Commit

---
+++ Exporter/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Exporter
+//
+//  Created by Meonardo on 2020/1/2.
+//  Copyright © 2020 Meonardo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
