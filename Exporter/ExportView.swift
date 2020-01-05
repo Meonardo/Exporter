4a95071e24f0d30781eab6107f1f2ff9b3cf78f0
Hedi <sisxme@foxmail.com>
2020-01-05 23:40:58 +0800

    alter ExportView

--- Exporter/ExportView.swift
+++ Exporter/ExportView.swift
@@ -60,14 +60,48 @@ struct SearchView: View {
 						.buttonStyle(TransparentButtonStyle())
 					}
 				}
+				
+				Button(action: {
+					
+				}) {
+					Text("搜索")
+				}
 			}
 			.padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
 			.foregroundColor(.secondary)
 			.background(Color(.controlBackgroundColor))
 			.cornerRadius(10.0)
 		}
-		.padding(.horizontal)
     }
+}
+
+struct SelectView: View {
+	
+	var title: String
+	var count: Int = 0
+	var selectedColor = Color.blue
+	
+	@State private var isSelected: Bool = false
+	
+	var body: some View {
+		ZStack {
+			HStack {
+				Text(title)
+					.font(Font.system(size: 18, weight: .medium))
+					.padding(10)
+				Spacer()
+				Text("\(count)")
+					.font(Font.system(size: 14))
+					.padding(.bottom, 30)
+					.padding(.trailing, 10)
+			}
+		}
+		.background(isSelected ? selectedColor : Color(.controlBackgroundColor))
+		.cornerRadius(10)
+		.onTapGesture {
+			self.isSelected.toggle()
+		}
+	}
 }
 
 struct ExportView: View {
@@ -75,6 +109,8 @@ struct ExportView: View {
 	@State private var buyerName = ""
 	
 	var backAction: (() -> Void)?
+	
+	private let viewWidth: CGFloat = 260
 	
     var body: some View {
 		HStack {
@@ -89,9 +125,17 @@ struct ExportView: View {
 				}.buttonStyle(TransparentButtonStyle())
 				
 				SearchView()
-					.frame(width: 240)
+					.frame(minHeight: 44)
+					.padding(.leading, 16)
+					
+				HStack(spacing: 8) {
+					SelectView(title: "客\n户")
+					SelectView(title: "商\n品", selectedColor: .orange)
+				}
+				.padding(.leading, 16)
+				
 				Spacer()
-			}
+			}.frame(width: viewWidth)
 			
 			VStack(alignment: .leading) {
 				Text("公司信息")
@@ -101,7 +145,10 @@ struct ExportView: View {
 					Text("公司信息")
 					Text("公司信息")
 				}
-			}.padding(.top, 12)
+				.cornerRadius(10)
+			}
+			.frame(minWidth: viewWidth + 100, minHeight: 400)
+			.padding(EdgeInsets(top: 4, leading: 8, bottom: 16, trailing: 16))
 		}.background(Color(.windowBackgroundColor))
     }
 	
